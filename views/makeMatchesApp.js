521744e91ee24866a450e9a4b654b048386c02da
Cher Moua <mouacher@pdx.edu>
2019-05-15 16:17:54 -0700

    able to catch the urlencoded data in server side, and properly redirect page from makeMatches to ThankYou.

--- views/makeMatchesApp.js
+++ views/makeMatchesApp.js
@@ -4,8 +4,7 @@ function postMatches(form){
   var xhttp = new XMLHttpRequest()
   xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-      //window.href.location = '/ThankYou'
-      alert("Thankyou")
+      window.location.href = '/ThankYou'
     }
   }
   xhttp.open(form.method, form.action, true)
